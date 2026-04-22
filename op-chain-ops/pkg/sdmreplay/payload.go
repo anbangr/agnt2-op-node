ethereum-optimism/optimism

feat(op-chain-ops): add Go sdmreplay client
Client for driving debug_replaySDMBlock over a range of blocks and
writing a JSONL trail of the replay results, for post-hoc verification
and cross-node consistency checks.

- ReplayRange iterates blocks with ctx cancellation support so long
  ranges can be aborted cleanly.
- DecodePayload RLP-decodes the 0x7D tx input and rejects unknown
  versions in lock-step with the Rust decoder (POST_EXEC_PAYLOAD_VERSION
  = 1). Cross-language drift would let a Go pipeline accept payloads
  the Rust node rejects.
- Unit tests cover the version check on both the current 3-field
  shape and the legacy 2-field fallback, plus an empty-input guard.
- Memory-buffered range mode is fine for devstack tests; streaming
  JSONL for mainnet-scale ranges is tracked as a pre-production item.

op-chain-ops/pkg/sdmreplay/payload.go
@@ -1,0 +1,85 @@
+package sdmreplay
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+const SDMTxType = 0x7d
+
+// PostExecPayloadVersion is the only PostExecPayload version the Go decoder accepts.
+// Must stay in lock-step with POST_EXEC_PAYLOAD_VERSION in rust/op-alloy, which rejects
+// unknown versions at decode time; Go accepting what Rust rejects is a cross-language
+// drift hazard on any replay/verifier pipeline sitting between the two.
+const PostExecPayloadVersion uint64 = 1
+
+// SDMGasEntry is one per-transaction refund entry inside the SDM portion of a post-exec payload.
+type SDMGasEntry struct {
+	Index     uint64 `json:"index"`
+	GasRefund uint64 `json:"gas_refund"`
+}
+
+// PostExecPayload is the decoded RLP payload carried by the synthetic post-exec tx.
+// Today this contains the SDM gas refund entries and the L2 block number the payload is anchored to.
+// Older payloads may omit BlockNumber.
+type PostExecPayload struct {
+	Version          uint64        `json:"version"`
+	BlockNumber      uint64        `json:"block_number,omitempty"`
+	GasRefundEntries []SDMGasEntry `json:"gas_refund_entries"`
+}
+
+// GasRefundForIndex returns the refund for the given block tx index.
+func (p *PostExecPayload) GasRefundForIndex(index uint64) (uint64, bool) {
+	if p == nil {
+		return 0, false
+	}
+	for _, entry := range p.GasRefundEntries {
+		if entry.Index == index {
+			return entry.GasRefund, true
+		}
+	}
+	return 0, false
+}
+
+// DecodePayload decodes an RLP-encoded post-exec payload from the post-exec tx input.
+func DecodePayload(input []byte) (*PostExecPayload, error) {
+	if len(input) == 0 {
+		return nil, fmt.Errorf("empty post-exec payload")
+	}
+
+	payload, err := decodePayloadStruct(input)
+	if err != nil {
+		return nil, err
+	}
+	if payload.Version != PostExecPayloadVersion {
+		return nil, fmt.Errorf(
+			"unsupported post-exec payload version %d (expected %d)",
+			payload.Version, PostExecPayloadVersion,
+		)
+	}
+	return payload, nil
+}
+
+// decodePayloadStruct tries the current RLP shape first, then falls back to the legacy
+// two-field shape. Version validation is applied by the caller so unknown versions are
+// rejected on either path.
+func decodePayloadStruct(input []byte) (*PostExecPayload, error) {
+	var payload PostExecPayload
+	if err := rlp.DecodeBytes(input, &payload); err == nil {
+		return &payload, nil
+	}
+
+	// Backward compatibility for older payloads that encoded only version + refund entries.
+	var legacy struct {
+		Version          uint64
+		GasRefundEntries []SDMGasEntry
+	}
+	if err := rlp.DecodeBytes(input, &legacy); err != nil {
+		return nil, fmt.Errorf("decode post-exec payload: %w", err)
+	}
+	return &PostExecPayload{
+		Version:          legacy.Version,
+		GasRefundEntries: legacy.GasRefundEntries,
+	}, nil
+}

op-chain-ops/pkg/sdmreplay/payload_test.go
@@ -1,0 +1,73 @@
+package sdmreplay
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDecodePayload_AcceptsCurrentShape(t *testing.T) {
+	payload := PostExecPayload{
+		Version:     PostExecPayloadVersion,
+		BlockNumber: 42,
+		GasRefundEntries: []SDMGasEntry{
+			{Index: 3, GasRefund: 7},
+			{Index: 5, GasRefund: 11},
+		},
+	}
+	encoded, err := rlp.EncodeToBytes(&payload)
+	require.NoError(t, err)
+
+	decoded, err := DecodePayload(encoded)
+	require.NoError(t, err)
+	require.Equal(t, &payload, decoded)
+}
+
+func TestDecodePayload_RejectsUnknownVersion(t *testing.T) {
+	// Any non-1 version must be rejected to stay in lock-step with the Rust decoder in
+	// rust/op-alloy, which gates on POST_EXEC_PAYLOAD_VERSION. Cross-language divergence
+	// here would let a Go-based replay pipeline accept payloads the Rust node rejects.
+	for _, version := range []uint64{0, 2, 99} {
+		t.Run("version_"+strconv.FormatUint(version, 10), func(t *testing.T) {
+			payload := PostExecPayload{
+				Version:          version,
+				BlockNumber:      1,
+				GasRefundEntries: []SDMGasEntry{{Index: 0, GasRefund: 1}},
+			}
+			encoded, err := rlp.EncodeToBytes(&payload)
+			require.NoError(t, err)
+
+			_, err = DecodePayload(encoded)
+			require.Error(t, err)
+			require.Contains(t, err.Error(), "unsupported post-exec payload version")
+		})
+	}
+}
+
+func TestDecodePayload_RejectsUnknownVersionOnLegacyShape(t *testing.T) {
+	// Same version check on the legacy two-field shape — the fallback decoder must not
+	// become an escape hatch for payloads whose version is wrong.
+	legacy := struct {
+		Version          uint64
+		GasRefundEntries []SDMGasEntry
+	}{
+		Version:          7,
+		GasRefundEntries: []SDMGasEntry{{Index: 0, GasRefund: 1}},
+	}
+	encoded, err := rlp.EncodeToBytes(&legacy)
+	require.NoError(t, err)
+
+	_, err = DecodePayload(encoded)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "unsupported post-exec payload version")
+}
+
+func TestDecodePayload_EmptyInputRejected(t *testing.T) {
+	_, err := DecodePayload(nil)
+	require.Error(t, err)
+
+	_, err = DecodePayload([]byte{})
+	require.Error(t, err)
+}

anbangr/agnt2-op-node

op-chain-ops/pkg/sdmreplay/payload.go
@@ -1,0 +1,85 @@
+package sdmreplay
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+const SDMTxType = 0x7d
+
+// PostExecPayloadVersion is the only PostExecPayload version the Go decoder accepts.
+// Must stay in lock-step with POST_EXEC_PAYLOAD_VERSION in rust/op-alloy, which rejects
+// unknown versions at decode time; Go accepting what Rust rejects is a cross-language
+// drift hazard on any replay/verifier pipeline sitting between the two.
+const PostExecPayloadVersion uint8 = 1
+
+// SDMGasEntry is one per-transaction refund entry inside the SDM portion of a post-exec payload.
+type SDMGasEntry struct {
+	Index     uint64 `json:"index"`
+	GasRefund uint64 `json:"gas_refund"`
+}
+
+// PostExecPayload is the decoded RLP payload carried by the synthetic post-exec tx.
+// Today this contains the SDM gas refund entries and the L2 block number the payload is anchored to.
+// Older payloads may omit BlockNumber.
+type PostExecPayload struct {
+	Version          uint8         `json:"version"`
+	BlockNumber      uint64        `json:"block_number,omitempty"`
+	GasRefundEntries []SDMGasEntry `json:"gas_refund_entries"`
+}
+
+// GasRefundForIndex returns the refund for the given block tx index.
+func (p *PostExecPayload) GasRefundForIndex(index uint64) (uint64, bool) {
+	if p == nil {
+		return 0, false
+	}
+	for _, entry := range p.GasRefundEntries {
+		if entry.Index == index {
+			return entry.GasRefund, true
+		}
+	}
+	return 0, false
+}
+
+// DecodePayload decodes an RLP-encoded post-exec payload from the post-exec tx input.
+func DecodePayload(input []byte) (*PostExecPayload, error) {
+	if len(input) == 0 {
+		return nil, fmt.Errorf("empty post-exec payload")
+	}
+
+	payload, err := decodePayloadStruct(input)
+	if err != nil {
+		return nil, err
+	}
+	if payload.Version != PostExecPayloadVersion {
+		return nil, fmt.Errorf(
+			"unsupported post-exec payload version %d (expected %d)",
+			payload.Version, PostExecPayloadVersion,
+		)
+	}
+	return payload, nil
+}
+
+// decodePayloadStruct tries the current RLP shape first, then falls back to the legacy
+// two-field shape. Version validation is applied by the caller so unknown versions are
+// rejected on either path.
+func decodePayloadStruct(input []byte) (*PostExecPayload, error) {
+	var payload PostExecPayload
+	if err := rlp.DecodeBytes(input, &payload); err == nil {
+		return &payload, nil
+	}
+
+	// Backward compatibility for older payloads that encoded only version + refund entries.
+	var legacy struct {
+		Version          uint8
+		GasRefundEntries []SDMGasEntry
+	}
+	if err := rlp.DecodeBytes(input, &legacy); err != nil {
+		return nil, fmt.Errorf("decode post-exec payload: %w", err)
+	}
+	return &PostExecPayload{
+		Version:          legacy.Version,
+		GasRefundEntries: legacy.GasRefundEntries,
+	}, nil
+}

op-chain-ops/pkg/sdmreplay/payload_test.go
@@ -1,0 +1,73 @@
+package sdmreplay
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDecodePayload_AcceptsCurrentShape(t *testing.T) {
+	payload := PostExecPayload{
+		Version:     PostExecPayloadVersion,
+		BlockNumber: 42,
+		GasRefundEntries: []SDMGasEntry{
+			{Index: 3, GasRefund: 7},
+			{Index: 5, GasRefund: 11},
+		},
+	}
+	encoded, err := rlp.EncodeToBytes(&payload)
+	require.NoError(t, err)
+
+	decoded, err := DecodePayload(encoded)
+	require.NoError(t, err)
+	require.Equal(t, &payload, decoded)
+}
+
+func TestDecodePayload_RejectsUnknownVersion(t *testing.T) {
+	// Any non-1 version must be rejected to stay in lock-step with the Rust decoder in
+	// rust/op-alloy, which gates on POST_EXEC_PAYLOAD_VERSION. Cross-language divergence
+	// here would let a Go-based replay pipeline accept payloads the Rust node rejects.
+	for _, version := range []uint8{0, 2, 99} {
+		t.Run("version_"+strconv.FormatUint(uint64(version), 10), func(t *testing.T) {
+			payload := PostExecPayload{
+				Version:          version,
+				BlockNumber:      1,
+				GasRefundEntries: []SDMGasEntry{{Index: 0, GasRefund: 1}},
+			}
+			encoded, err := rlp.EncodeToBytes(&payload)
+			require.NoError(t, err)
+
+			_, err = DecodePayload(encoded)
+			require.Error(t, err)
+			require.Contains(t, err.Error(), "unsupported post-exec payload version")
+		})
+	}
+}
+
+func TestDecodePayload_RejectsUnknownVersionOnLegacyShape(t *testing.T) {
+	// Same version check on the legacy two-field shape — the fallback decoder must not
+	// become an escape hatch for payloads whose version is wrong.
+	legacy := struct {
+		Version          uint8
+		GasRefundEntries []SDMGasEntry
+	}{
+		Version:          7,
+		GasRefundEntries: []SDMGasEntry{{Index: 0, GasRefund: 1}},
+	}
+	encoded, err := rlp.EncodeToBytes(&legacy)
+	require.NoError(t, err)
+
+	_, err = DecodePayload(encoded)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "unsupported post-exec payload version")
+}
+
+func TestDecodePayload_EmptyInputRejected(t *testing.T) {
+	_, err := DecodePayload(nil)
+	require.Error(t, err)
+
+	_, err = DecodePayload([]byte{})
+	require.Error(t, err)
+}